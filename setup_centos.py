ae200494af16c0540054f0d438de61f46b954933
John Lambert <johnlambert@gatech.edu>
2021-08-24 13:58:12 -0600

    fix typo in hard-coded cmake eigen args

--- setup_centos.py
+++ setup_centos.py
@@ -33,7 +33,7 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir, '-DEIGEN3_INCLUDE_DIRS=/eigen-3.3.9'
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir, '-DEIGEN3_INCLUDE_DIRS=/eigen-3.3.9',
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
