57687a0d0f4aada5a29189bf84c3a092fec1a25a
John Lambert <johnlambert@gatech.edu>
2021-08-23 19:47:03 -0600

    use yum install

--- build-wheels-linux.sh
+++ build-wheels-linux.sh
@@ -6,6 +6,7 @@
 
 # declare -a PYTHON_VERSION=( $1 )
 
+# we cannot simply use `pip` or `python`, since points to old 2.7 version
 PYBIN="/opt/python/$PYTHON_VERSION/bin"
 PYVER_NUM=$($PYBIN/python -c "import sys;print(sys.version.split(\" \")[0])")
 PYTHONVER="$(basename $(dirname $PYBIN))"
@@ -18,12 +19,12 @@ export PATH=$PYBIN:$PATH
 
 ${PYBIN}/pip install auditwheel
 
-# which python
-# python --version
+PYTHON_EXECUTABLE=${PYBIN}/python
+# We use distutils to get the include directory and the library path directly from the selected interpreter
+# We provide these variables to CMake to hint what Python development files we wish to use in the build.
+PYTHON_INCLUDE_DIR=$(${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+PYTHON_LIBRARY=$(${PYTHON_EXECUTABLE} -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 
-# which pip
-# pip --version
-# pip install auditwheel
 
 CURRDIR=$(pwd)
 COLMAP_BRANCH="dev"
@@ -33,7 +34,7 @@ echo "Num. processes to use for building: ${nproc}"
 apt-get update
 
 # ----------- Install dependencies from the default Ubuntu repositories -----------------
-apt-get install \
+yum install \
     git \
     cmake \
     build-essential \
@@ -51,12 +52,12 @@ apt-get install \
     qtbase5-dev \
     libqt5opengl5-dev \
     libcgal-dev
-apt-get install libcgal-qt5-dev
+yum install libcgal-qt5-dev
 
 # ----------- Install CERES solver -------------------------------------------------------
-apt install libeigen3-dev # was not in COLMAP instructions
-apt-get install libatlas-base-dev libsuitesparse-dev
-apt-get install libgoogle-glog-dev libgflags-dev # was not in COLMAP instructions
+yum install libeigen3-dev # was not in COLMAP instructions
+yum install libatlas-base-dev libsuitesparse-dev
+yum install libgoogle-glog-dev libgflags-dev # was not in COLMAP instructions
 git clone https://ceres-solver.googlesource.com/ceres-solver
 cd ceres-solver
 git checkout $(git describe --tags) # Checkout the latest release
@@ -77,17 +78,6 @@ git checkout dev
 # mkdir $BUILDDIR
 # cd $BUILDDIR
 
-PYBIN="/opt/python/$PYTHON_VERSION/bin"
-PYVER_NUM=$($PYBIN/python -c "import sys;print(sys.version.split(\" \")[0])")
-PYTHONVER="$(basename $(dirname $PYBIN))"
-
-export PATH=$PYBIN:$PATH
-
-PYTHON_EXECUTABLE=${PYBIN}/python
-# We use distutils to get the include directory and the library path directly from the selected interpreter
-# We provide these variables to CMake to hint what Python development files we wish to use in the build.
-PYTHON_INCLUDE_DIR=$(${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
-PYTHON_LIBRARY=$(${PYTHON_EXECUTABLE} -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 
 echo ""
 echo "PYTHON_EXECUTABLE:${PYTHON_EXECUTABLE}"
@@ -98,25 +88,25 @@ echo ""
 # ---------- Fix broken dependencies -----
 
 # try new boost install
-apt-get install libboost-all-dev
-apt-get install git
-apt-get install cmake
-apt-get install build-essential
-apt-get install libboost-program-options-dev
-apt-get install libboost-filesystem-dev
-apt-get install libboost-graph-dev
-apt-get install libboost-system-dev
-apt-get install libboost-test-dev
-apt-get install libeigen3-dev
-apt-get install libsuitesparse-dev
-apt-get install libfreeimage-dev
-apt-get install libgoogle-glog-dev
-apt-get install libgflags-dev
-apt-get install libglew-dev
-apt-get install qtbase5-dev
-apt-get install libqt5opengl5-dev
-apt-get install libcgal-dev
-apt-get install libcgal-qt5-dev
+yum install libboost-all-dev
+yum install git
+yum install cmake
+yum install build-essential
+yum install libboost-program-options-dev
+yum install libboost-filesystem-dev
+yum install libboost-graph-dev
+yum install libboost-system-dev
+yum install libboost-test-dev
+yum install libeigen3-dev
+yum install libsuitesparse-dev
+yum install libfreeimage-dev
+yum install libgoogle-glog-dev
+yum install libgflags-dev
+yum install libglew-dev
+yum install qtbase5-dev
+yum install libqt5opengl5-dev
+yum install libcgal-dev
+yum install libcgal-qt5-dev
 
 
 # ----------- Build COLMAP ------------------------------------------------------------
@@ -143,7 +133,7 @@ git clone --recursive https://github.com/mihaidusmanu/pycolmap.git
 cd pycolmap
 PLAT=manylinux2014_x86_64
 #"${PYBIN}/python" setup.py bdist_wheel --python-tag=$PYTHONVER --plat-name=$PLAT
-python3 setup.py bdist_wheel --plat-name=$PLAT #--python-tag=$PYTHONVER 
+"${PYBIN}/python" setup.py bdist_wheel --plat-name=$PLAT #--python-tag=$PYTHONVER 
 
 cp ./dist/*.whl /io/wheelhouse/
 
