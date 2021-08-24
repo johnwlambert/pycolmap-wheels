109eade87abbbb9ff2e9888ce779f14dddbee593
John Lambert <johnlambert@gatech.edu>
2021-08-24 07:15:24 -0600

    add retry

--- build-wheels-linux.sh
+++ build-wheels-linux.sh
@@ -6,6 +6,26 @@
 
 # declare -a PYTHON_VERSION=( $1 )
 
+function retry {
+  local retries=$1
+  shift
+
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** $count))
+    count=$(($count + 1))
+    if [ $count -lt $retries ]; then
+      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep $wait
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      return $exit
+    fi
+  done
+  return 0
+}
+
 uname -a
 echo "Current CentOS Version:"
 cat /etc/centos-release
